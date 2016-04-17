b06b77d28af7eb77f2478d6da35cc758b8bbf63d
hwangmn <hwangmn@bc.edu>
2016-04-17 12:05:34 -0400

    First Commit for Loggin In and Sign Up For New ID
    ;

---
+++ dboperation.php
@@ -0,0 +1,84 @@
+<?php
+include('dbconn.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="../club/css/style10.css"/>
+	<script type="text/javascript"src="valid.js"></script>
+	<meta charset="utf-8" />
+	<title>Sign Up</title>
+</head>
+
+<body>
+
+<?php
+
+	if ( isset( $_POST['submit'] ) )
+		handle_form( $_POST['email'] );
+?>
+<fieldset>
+	<legend> Sign Up To Create Your Own Calorie Log! </legend>
+
+	<form method= 'post' name = "myForm" onsubmit = "return validateForm()">
+		Username:
+		<input type = 'text' name= 'name' size= '40'> <br>
+		E-mail:
+		<input type = 'text' name= 'email' size= '40'> <br>
+		Password:
+		<input type = 'password' name= 'pw1' size= '40'> <br>
+		Re-Type Password:
+		<input type = 'password' name= 'pw2' size= '40'> <br>
+
+		<input type='submit' value = "SUBMIT!" name = 'submit'> <br>
+	</form>
+</fieldset>
+</body>
+</html>
+
+<?php
+function handle_form( $id ){
+
+	$dbc    = connect_to_db( "hwangmn" );
+	$query  = "select * from food";
+	$result = perform_query( $dbc, $query );
+
+	$username = $_POST['name'];
+	$useremail = $_POST['email'];
+	$userpw1 = $_POST['pw1'];
+	$userpw2 = $_POST['pw2'];
+
+	if ($userpw1 != $userpw2) {
+		echo "Your Passwords Did Not Match. Please Try Again!";
+		return;
+	}
+
+	else {
+		while ($row = mysqli_fetch_array( $result, MYSQLI_ASSOC )){
+
+			$name  = $row['username'];
+			$email = $row['email'];
+			$pw    = $row['password'];
+
+				if ($useremail == $email) {
+					echo "Sorry! This E-mail Address has already been used!
+					<form action='http://cscilab.bc.edu/~hwangmn/hw10/club/index.php'>
+					<input type='submit' value='Go Back'>
+					</form>";
+
+					disconnect_from_db( $dbc, $result );
+					return;
+				}
+		}
+	}
+
+	$newquery = "INSERT INTO `food`(`username`, `email`, `password`, `regidate`) VALUES ('$username', '$useremail', sha('$userpw1'), now())";
+	perform_query( $dbc, $newquery );
+	echo "Thank You For Joining the Club!
+		<form action='http://cscilab.bc.edu/~hwangmn/final/login.php'>
+		<input type='submit' value='Go Back'>
+		</form><br/>";
+
+	disconnect_from_db( $dbc, $result);
+}
+?>
---
+++ login.php
@@ -0,0 +1,148 @@
+<?php
+include('dbconn.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style10.css"/>
+	<script type="text/javascript"src="../include/valid.js"></script>
+	<meta charset="utf-8" />
+	<title>Welcome To Calorie Calculator</title>
+</head>
+<body>
+	<pre><?php //print_r($_POST); ?></pre>
+<fieldset>
+	<h1>Diet Is Fun!</h1>
+	<img src="diet.jpg" alt="food pic" height="360" width="600">
+
+	<form action='dboperation.php'>
+		<input type="submit" name = "join" value= "Don't Have an Account? Sign Up!">
+	</form>
+
+	<fieldset>
+		<legend> Please Login to Access Your Calorie Log! </legend>
+
+	<form method= 'post' name = 'myForm' onsubmit = 'return validateForm2()'>
+			E-mail:
+			<input type = 'text' name= 'email' size= '40'> <br>
+			PassWord:
+			<input type = 'text' name= 'pw' size= '40'> <br>
+
+			<input type='submit' value = 'Login' name = 'submit'> <br>
+	</form>
+
+	<form method = 'post'>
+			<input type="submit" name = "pwlost" value= "Forgot Your Password?">
+	</form>
+
+	<?php
+
+			if ( isset( $_POST['pwlost'] ) )
+				display_form();
+	?>
+
+	</fieldset>
+
+	<?php
+
+		if ( isset( $_POST['submit'] ) )
+			handle_form( $_POST['email'], $_POST['pw'] );
+	?>
+
+	<?php
+
+		if ( isset( $_POST['pwsubmit'] ) )
+			handle_form1( $_POST['email1']);
+	?>
+
+</fieldset>
+
+</body>
+</html>
+
+<?php
+function display_form () {
+	echo "<form method= 'post' name = 'myForm' onsubmit = 'return validateForm2()'>
+		Please Give Us Your E-mail: <br>
+		<input type = 'text' name= 'email1' size= '40'> <br>
+		<input type='submit' value = 'SUBMIT!' name = 'pwsubmit'> <br>
+	</form>";
+}
+?>
+
+<?php
+function handle_form( $id, $pw){
+
+	$dbc    = connect_to_db( "hwangmn" );
+	$query  = "select * from food";
+	$result = perform_query( $dbc, $query );
+	//print_r($row);
+
+	$useremail = $_POST['email'];
+	$userpw = $_POST['pw'];
+
+	while ($row = mysqli_fetch_array( $result, MYSQLI_ASSOC )){
+
+		$email = $row['email'];
+		$pw = $row['password'];
+
+		if ($useremail == $email) {
+			if(sha1($userpw) == $pw) {
+				echo "Log in Successful!";
+			}
+			else {
+				echo "Wrong Password!";
+			}
+			return;
+		}
+	}
+
+	echo "Username Not Found!";
+
+	disconnect_from_db( $dbc, $result);
+	return;
+}
+
+function handle_form1( $id ){
+
+	$dbc    = connect_to_db( "hwangmn" );
+	$query  = "select * from food";
+	$result = perform_query( $dbc, $query );
+	//print_r($row);
+
+	$useremail = $_POST['email1'];
+
+	while ($row = mysqli_fetch_array( $result, MYSQLI_ASSOC )){
+
+		$email = $row['email'];
+
+		if ($useremail == $email) {
+			echo "Hi! Thanks for the enquery! Your Password will be sent to you shortly to your e-mail!";
+
+			$newpw = newpass();
+			$query1 = "UPDATE `food` SET `password` = sha1('$newpw') WHERE `email` = '$useremail' ";
+			perform_query($dbc, $query1);
+
+			mail("$useremail", "Your New Calorie Log Account Password!", $newpw);
+
+			disconnect_from_db( $dbc, $result );
+			return;
+		}
+	}
+
+	echo "Hi! We could not find your e-mail... Are you Sure You've Signed up Already?";
+
+	disconnect_from_db( $dbc, $result);
+	return;
+}
+
+function newpass() {
+	$pw = "";
+	$pwchar = "asdfghjkl1234567890";
+	for ($i = 0; $i < 7; $i ++) {
+		$pw .= $pwchar[rand(0, strlen($pwchar)-1)];
+	}
+	return $pw;
+}
+?>
