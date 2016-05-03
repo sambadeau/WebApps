b6c9d5816be7a3de6cffe7f2936c6a6be2e09bc7
wudh1 <wudh@bc.edu>
2016-05-03 14:45:44 -0400

    Search bar, delete history, more exercises

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
-function calculate(a, b, c, d, e, f, g, newone) {
-	if (newone != "Select an exercise"){
+function calculate(a, b, c, d, e, f, g, h, newone) {
+	if (newone != "Select an exercise" && h != -10){
 		var total;
-		total = a + b + c + d + e + f + g;
+		total = a + b + c + d + e + f + g + h;
 		if (newone == "Run") {
 			run(total);
 		}
@@ -11,13 +11,35 @@ function calculate(a, b, c, d, e, f, g, newone) {
 		if (newone == "Swim") {
 			swim(total);
 		}
+		if (newone == "Tennis") {
+			tennis(total);
+		}
+		if (newone == "Walk") {
+			walk(total);
+		}
+	}
+	else if (h == -10) {
+		document.getElementById("result").innerHTML = "Food not found in database - try again";
 	}
 	else {
 		document.getElementById("result").innerHTML = "You didn't select an exercise!";
 	}
 
 }
+function walk(x) {
+	var a = parseInt(x);
+	var r = (a) / (25/6);
+	var real = r.toFixed(2);
+	document.getElementById("result").innerHTML = "You have to walk (about 3 mph) for " + real + " minutes to burn off that item(s)";
 
+}
+function tennis(x) {
+	var a = parseInt(x);
+	var r = (a) / 5;
+	var real = r.toFixed(2);
+	document.getElementById("result").innerHTML = "You have play tennis (singles) on average for " + real + " minutes to burn off that item(s)";
+
+}
 function run(x) {
 	var a = parseInt(x);
 	var r = (a) / (52/19);
