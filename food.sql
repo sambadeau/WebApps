8250729cfadb21a7cf1261459483f7aa7c0f664a
sambadeau <badeau@bc.edu>
2016-05-03 19:55:13 -0400

    updated

--- food.sql
+++ food.sql
@@ -65,6 +65,8 @@ INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Oatmeal Crais
 INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Plain Croissants', '284', 'Bakery');
 INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Carved Marinated Flank Steak Sandwich and fries', '1282', 'Dinner');
 INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Garlic Mashed Potatoes', '291', 'Dinner');
+
+--this stuff is new-- 
 INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Coffee', '621', 'Dinner');
 INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Coffee', '621', 'Dinner');
 INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Coffee', '621', 'Dinner');
@@ -85,75 +87,3 @@ INSERT INTO `hwangmn`.`food` (`Food`, `Calories`, `Type`) VALUES ('Coffee', '621
 
 
 
-
-
-
-Blueberry & Cream Cheese Croissant	2275	Bakery
-Belgian Waffle	442	Breakfast
-Eggs cooked your way	103	Breakfast
-Pancakes	290	Breakfast
-Sausage Pattys	213	Breakfast
-Bagel Sandwich	608	Breakfast
-Egg McBc	551	Breakfast
-Pancake Value	1001	Breakfast
-Hot Oatmeal 10 oz	179	Breakfast
-Bacon	393	Breakfast
-Crispy Breakfast Potatoes	220	Breakfast
-Hard Boiled Egg	72	Breakfast
-Sausage Egg & Cheese Biscuit	618	Breakfast
-Steak & Egg Sub	510	Breakfast
-Omelet	396	Breakfast
-Baby Arugula Parmesan Salad	641	Grab and Go
-Cheese Tray	558	Grab and Go
-Chicken Pesto Pasta Salad	618	Grab and Go
-Chickpea Avacado on Grilled Flat Bread	500	Grab and Go
-Chipotle Roast Beef Wrap	916	Grab and Go
-BC Chocolate Chip Cookie (Dozen)	2258	Bakery
-Fluffer Club With Nutella	573	Grab and Go
-G.F. Whole Grain Peanut Butter& Jelly	385	Grab and Go
-Gluten Free Whole Grain Turkey Sandwich	599	Grab and Go
-Italian Sub	681	Grab and Go
-Seared Balsamic Chicken Salad	410	Grab and Go
-Tomato Mozzarella Sandwich	630	Grab and Go
-Turkey Sandwich on Multigrain	574	Grab and Go
-Caramel Heath Crunch Cake	6674	Bakery
-Carrot Cupcake w/ Cream Cheese Icing (Dozen)	8897	Bakery
-Cho Drizzle Coconut Cookie	2950	Bakery
-Choc Image Cake	6457	Bakery
-Chocolate Croissant	360	Bakery
-Coffee Roll	621	Bakery
-Molasses Cookies (Dozen)	2847	Bakery
-Oatmeal Craisin Cookie (Dozen)	3643	Bakery
-Plain Croissants	284	Bakery
-BC Burger Value Meal	813	Lunch
-Gardenburger Value Meal
-640	Lunch
-Grilled Cheese & Soup Value Meal	703	Lunch
-Grilled Chicken Sandwich Value Meal	683	Lunch
-GPizza Value Meal	942	Lunch
-Soup and 1/2 Sandwich Value Meal	626	Lunch
-Vegetarian Entree Value Meal	387	Lunch
-Beef Chili	361	Lunch
-Chicken Noodle Soup	114	Lunch
-Quinoa & Sweet Potato Chili	267	Lunch
-Roasted Vegetable Soup	210	Lunch
-Cold Wraps	550	Lunch
-Deli Sandwich	357	Lunch
-Large Cold Subs	1085	Lunch
-Regular Cold Sub	519	Lunch
-Chicken Philly Cheese Steak Quesadilla	1257	Lunch
-Grilled Vegetable Quesadilla	1589	Lunch
-Philly Cheese Steak Quesadilla	1093	Lunch
-Noodle Bar	603	Lunch
-Cheese Pizza	447	Lunch
-Meatball Pizza	460	Lunch
-Pepperoni Pizza	495	Lunch
-Whole Wheat Three Cheese Broccoli Pizza	338	Lunch
-BC Burger with Toppings	603	Lunch
-Garden Burger
-431	Lunch
-Grilled Chicken Sandwich	473	Lunch
-Grilled Portabella Sandwich	412	Lunch
-Sirloin Steak Sandwich	431	Lunch
-Select an option	0	Grab and Go
-Select an option	0	Bakery
